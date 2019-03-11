c7f3795b17450754171f481c427bb90ba360c9fe
yaozong <hpps9603@gmail.com>
2019-03-11 17:05:57 +0800

    comparison the order of reg to know the architecture

--- main.s
+++ main.s
@@ -6,20 +6,20 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	// write the value of # to reg
+	movs 	r0,	#100
+	movs	r1,	#101
+	movs	r2,	#102
 
-	//
-	//branch w/o link
-	//
-	b	label01
+	// the reference set 
+	push	{r0,r1,r2}
+	pop	{r3,r4,r5}
+	
+	// the comparison set
+	push	{r2,r0,r1}
+	pop	{r6,r7,r8}
 
-label01:
-	nop
-
-	//
-	//branch w/ link
-	//
-	bl	sleep
+	b	sleep
 
 sleep:
 	nop
